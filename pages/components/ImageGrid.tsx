2601ad8abc36f9588883de0194793b07fab95bb5
danilobatson <djbatson19@gmail.com>
2022-12-09 05:58:55 -0800

    adds ImageGrid

---
+++ pages/components/ImageGrid.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { ImageType } from '../pages/';
+
+interface ImageGridProps {
+  image: ImageType;
+}
+const ImageGrid: React.FC<ImageGridProps> = ({ image }) => {
+  const { imageSrc, photoName } = image;
+  return (
+    <div className={styles.card}>
+      <h5>{photoName}</h5>
+      <Image src={imageSrc} width={200} height={200} alt='Uploaded image' />
+    </div>
+  );
+};
+
+export default ImageGrid;
