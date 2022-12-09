c9bfe01ec63ef7ce163de341a363090727703e9d
danilobatson <djbatson19@gmail.com>
2022-12-09 05:59:36 -0800

    adds component index

---
+++ pages/components/index.ts
@@ -0,0 +1,6 @@
+import ImageGrid from "./ImageGrid";
+import ImageUpload from "./ImageUpload";
+import InputUpload from "./InputUpload";
+import Description from "./Description";
+
+export { ImageGrid, Description, ImageUpload, InputUpload}
