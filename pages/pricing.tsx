87914870a6e8d49ae3bf0d40ba044c90dda2f66c
danilobatson <djbatson19@gmail.com>
2022-12-12 01:14:17 -0800

    removes footer

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -211,36 +211,6 @@ function PricingContent() {
           ))}
         </Grid>
       </Container>
-      {/* Footer */}
-      <Container
-        maxWidth='md'
-        component='footer'
-        sx={{
-          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
-          mt: 8,
-          py: [3, 6],
-        }}
-      >
-        <Grid container spacing={4} justifyContent='space-evenly'>
-          {footers.map((footer) => (
-            <Grid item xs={6} sm={3} key={footer.title}>
-              <Typography variant='h6' color='text.primary' gutterBottom>
-                {footer.title}
-              </Typography>
-              <ul>
-                {footer.description.map((item) => (
-                  <li key={item}>
-                    <Link href='#' variant='subtitle1' color='text.secondary'>
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </Grid>
-          ))}
-        </Grid>
-      </Container>
-      {/* End footer */}
     </React.Fragment>
   );
 }
